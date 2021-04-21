9eaa9831a0154ac228a1d5546491552f31c120d1
Hideaki Kondo <hdaknd+github@gmail.com>
2021-04-22 02:36:18 +0900

    add load button

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,16 @@
         </div>
       </div>
     </div>
+    <div class="block">
+      <nav class="pagination">
+        <a
+        href.prevent="#" 
+        class="pagination-next"
+        @click="loadMore">
+          More
+        </a>
+      </nav>
+    </div>
   </section>
 </template>
 
@@ -23,6 +33,21 @@
     computed: {
       items() {
         return this.$store.getters.getPopularVideos
+      },
+      nextPageToken() {
+        return this.$store.getters.getMeta.nextPageToken
+      }
+    },
+    methods: {
+      loadMore() {
+        const payload = {
+          uri: ROUTES.GET.POPULARS,
+          params: {
+            pageToken: this.nextPageToken
+          }
+        }
+
+        this.$store.dispatch('fetchPopularVideos', payload)
       }
     },
 
