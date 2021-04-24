e7b2072b44b6cf59dccb045cde97566dddfcfced
Hideaki Kondo <hdaknd+github@gmail.com>
2021-04-24 22:59:05 +0900

    add search view

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -8,5 +8,23 @@
             YouTube Player
             </nuxt-link>
         </div>
+        <div class="navbar-menu">
+            <div class="navbar-start">
+                <div class="navbar-item">
+                    <div class="find has-addons">
+                        <form action="/search" method="get">
+                            <div class="find has-addons">
+                                <div class="control">
+                                    <input name="q" class="input" type="text" placeholder="検索">
+                                </div>
+                                <div class="control">
+                                    <button class="button">Search</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </nav>
 </template>
